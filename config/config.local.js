59976a5141a251c08bf5a9510895052e4aebf7c5
rejiejay <454766952@qq.com>
2019-07-05 07:42:18 +0800

    切换到 nuxtjs

--- config/config.local.js
+++
@@ -1,8 +0,0 @@
-// 测试环境配置
-export default  {
-
-    url: {
-        // 请求源
-        origin: 'http://localhost:1938',
-    }
-};
--- config/config.prod.js
+++
@@ -1,8 +0,0 @@
-// 生产环境配置
-export default  {
-
-    url: {
-        // 请求源  
-        origin: `${window.location.origin}/server`,
-    }
-};
